2eb8915c01b8a79603a8cccce2e01e8496186b91
Kanakadurga <kanaka@git.training>
2017-10-02 10:35:25 -0400

    used stingUtils and added dependency to lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
       
       public Application() {
@@ -7,10 +9,16 @@ public class Application {
  	  // method main(): ALWAYS the APPLICATION entry point
  
      // method main(): ALWAYS the APPLICATION entry point
+	 public int countWords(String words){
+		 String[] seperateWords = StringUtils.split(words, ' ');
+		 return(seperateWords == null)? 0 : seperateWords.length;
+	 }
       public static void main (String[] args) {
  		  System.out.println ("Starting Application");
  		  Application app = new Application();
      	System.out.println ("Starting Application");
- 	Application app1 = new Application();
+		int count = app.countWords("I have four words");
+		System.out.println("words count:" + count);
+		
       }
   }
